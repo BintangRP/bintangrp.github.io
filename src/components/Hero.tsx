d6b914eed09e48f39502848fb07f37b39c423d21
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2025-06-03 22:35:40 +0700

    del: icons deleted

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,3 @@
-import { FaReadme } from "react-icons/fa";
-
 const Hero = () => {
     return (
         <section id="heroSection" className="min-h-screen flex items-center justify-center text-center p-6">
